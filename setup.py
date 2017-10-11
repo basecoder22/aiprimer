bbd6697cbf3437733a74c660bb971072df3ef7ec
basecoder22 <blackhat!12>
2017-10-11 09:55:29 -0400

    Added setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###################################################
+# Created By: basecoder22
+###################################################
+from setuptools import setup
+
+setup(name='aiprimer',
+      version='1.0',
+      description='AI primer project',
+      author='Basecoder22',
+      author_email='basecoder22@gmail.com',
+      packages=['aiprimer'],
+      )
+
+__author__ = 'Basecoder22'
